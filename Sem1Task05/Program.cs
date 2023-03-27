279f4aceffe0054f8bee0716bd4c66a414228ad3
Veronica Charniak <veronicachechco@gmail.com>
2023-03-27 21:08:04 +0300

    Sem1Task05

---
+++ Sem1Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+string? numNLine = Console.ReadLine();
+if(numNLine != null)
+{
+    int numN = int.Parse(numNLine);
+    int startNum = numN * (-1);
+
+    string res = string.Empty;
+
+    while(startNum <= numN)
+    {
+        res = res + startNum + " ";
+        startNum = startNum + 1;
+    }
+     
+    Console.WriteLine(res);
+}
