5bdc529cdc0e76f596462e6f4aef847257fa37db
Veronica Charniak <veronicachechco@gmail.com>
2023-03-27 21:22:53 +0300

    Sem1Task07

---
+++ Sem1Task07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+string? numLine = Console.ReadLine();
+if(numLine != null)
+{
+    int num = int.Parse(numLine);
+    int res = num % 10;
+    Console.WriteLine(res);
+}
