1acf4464bb9f46f5f9a3a57bbcd9fcb8d2b0d1c6
Veronica Charniak <veronicachechco@gmail.com>
2023-03-29 23:25:49 +0300

    Sem2Task16

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. 
+// Способ 1 - при помощи метода
+
+int n1 = int.Parse(Console.ReadLine()??"0");
+int n2 = int.Parse(Console.ReadLine()??"0");
+
+void SqrtTest(int a, int b)
+{
+if (a == b*b)
+    Console.WriteLine(a + " квадрат " + b);
+else
+    Console.WriteLine(a + " не квадрат " + b);
+}
+
+//Второй способ
+int n1 = int.Parse(Console.ReadLine()??"0");
+int n2 = int.Parse(Console.ReadLine()??"0");
+
+if ((n1==n2*n2)||(n2==n1*n1))
+{
+    Console.WriteLine("является");
+}
+
+else
+{
+    Console.WriteLine("не является");
+}
