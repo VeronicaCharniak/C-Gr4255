2818391ca1d1c268f40474a3cc39302d7b1fdfce
Veronica Charniak <veronicachechco@gmail.com>
2023-04-05 22:49:28 +0300

    Sem3Task26

---
+++ Sem3Task26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.Clear();
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+void PrintResult(string message, long res)
+{
+     Console.WriteLine(message+res);
+}
+
+int CountDigit(int number)
+{
+    int result = 0;
+    while(number>0)
+    {
+      result=result +1;
+      number = number/10;              
+    }
+    return result ;
+}
+
+int numberA = ReadData("put number a: ");
+int res1 = CountDigit (numberA );
+PrintResult ("count:",(int)res1);
