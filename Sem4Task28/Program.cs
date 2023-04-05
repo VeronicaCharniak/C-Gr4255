78b607c617f613f79b58745c61a99b49c5cd8312
Veronica Charniak <veronicachechco@gmail.com>
2023-04-05 22:49:56 +0300

    Sem4Task28

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+using System.Numerics;
+
+Console.Clear();
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+void PrintResult(string message, BigInteger res)
+{
+     Console.WriteLine(message+res);
+}
+
+BigInteger Factorial (int number)
+{
+    BigInteger result = 1;               
+    for (int i = 1; i<=number; i++)
+    {     
+         result = result * i;
+    }
+    return result;                
+}
+
+int numberA = ReadData("put number a: ");
+BigInteger res1 = Factorial (numberA );
+PrintResult ("count:",res1);
