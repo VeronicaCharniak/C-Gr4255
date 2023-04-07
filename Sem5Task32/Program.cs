f3a21f4c72fd1269bd32ad7638bc32776ea9f185
Veronica Charniak <veronicachechco@gmail.com>
2023-04-07 22:42:44 +0300

    Sem5 31, 32, 33, 35, 37

---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу замена элементов массива:
+//положительные элементы замените на
+//соответствующие отрицательные, и наоборот
+//Ввод длины массива
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+//Метод печати одномерного массива
+void Print1Darray(int []arr)
+{
+    Console.Write("[");
+    for(int i = 0; i<arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+",");
+    }
+    Console.WriteLine(arr[arr.Length-1] + "]");
+}
+
+//Заполнение массива
+int[] Gen1DArray(int len, int top, int but)
+{
+    int[] res=new int[len];
+    for(int i=0; i<len; i++)
+    {
+        res[i]= new Random().Next(but, top+1);
+    }
+    return res;
+}
+//инвертируем массив
+int[] InvertArr(int[] arr, int len)
+{
+    for(int i=0; i<len; i++)
+    {
+        arr[i] = arr[i]*(-1);
+    }
+    return arr;
+}
+
+void InversArrRef(int[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        arr[i] = arr[i]*(-1);
+    }
+}
+
+int lenArr = ReadData("Введите длину массива: ");
+int[] arr = Gen1DArray(lenArr,999,-999);
+Print1Darray(arr);
+InversArrRef(arr);
+Print1Darray(arr);
