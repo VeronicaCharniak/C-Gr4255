d526226227fc42c792bc797a2914442690720444
Veronica Charniak <veronicachechco@gmail.com>
2023-04-13 23:06:46 +0300

    Sem 6 Tasks 41, 43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Метод, выполняющий чтение данных с консоли
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+//Метод, подсчитывающий количество положительных чисел
+int PositiveCounter (int num)
+{
+    int count = 0;
+    while(num > 0)
+    {
+        int value = ReadData("Введите число: ");
+        if(value > 0) count++;
+        num = num - 1;
+    }
+    return count;
+}
+
+int number = ReadData("Сколько чисел вы хотите ввести: ");
+int result = PositiveCounter(number);
+Console.WriteLine("Количество введенных вами положительных чисел составило: " + result);
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//Метод, выполняющий чтение данных с консоли
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+int CrossPointX(int k1, int b1, int k2, int b2)
+{
+        int x = (b2 - b1) / (k1 - k2);
+        return x;
+}
+
+int CrossPointY(int k1, int b1, int k2, int b2)
+{
+        int y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+        return y;
+}
+
+int k1Value = ReadData("Введите значение k1: ");
+int b1Value = ReadData("Введите значение b1: ");
+int k2Value = ReadData("Введите значение k2: ");
+int b2Value = ReadData("Введите значение b2: ");
+
+if((k1Value - k2Value) == 0)
+{
+    Console.Write("Разница k1 и k2 равняется нулю. А на ноль делить нельзя. Введите другие значения.");
+}
+if(k1Value == k2Value && b1Value == b2Value)
+{
+    Console.Write("Прямые совпадают");
+}
+if(k1Value == k2Value && b1Value != b2Value)
+{
+    Console.Write("Прямые параллельны");
+}
+else
+{
+int xValue = CrossPointX(k1Value, b1Value, k2Value, b2Value);
+int yValue = CrossPointY(k1Value, b1Value, k2Value, b2Value);
+Console.WriteLine("Заданные прямые пересекаются в точке с координатами: x (" + xValue + ") и y (" + yValue + ")");
+}
+
+
