70e9f588d200ff0f7c8e345495a1aca5ad84c3b8
Veronica Charniak <veronicachechco@gmail.com>
2023-04-17 23:20:51 +0300

    47: двумерный массив вещественных чисел

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//Метод, выполняющий чтение данных с консоли
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+// Метод, генерирующий массив вещественных чисел
+double[,] Gen2DArray(int rows, int columns, int bot, int top)
+{
+    double[,] arr = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i,j] = new Random().Next(bot, top + 1) + new Random().NextDouble();
+        }
+    }
+    return arr;
+}
+
+// метод печати двумерного массива
+void Print2DArray(double[,] arr)
+{
+    // раскрашивание массива
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0,16)]; //задание цвета шрифту
+            Console.Write(arr[i,j]+" "); // печать в 1 строку
+            Console.ResetColor(); // сбрасывание цвета
+        }
+        Console.WriteLine(); // переход на новую строку по окончании цикла по столбцам (j)
+    }
+}
+
+int testRows = ReadData("Введите количество строк в двумерном массиве: ");
+int testColumns = ReadData("Введите количество столбцов в двумерном массиве: ");
+double[,] testArr2D = Gen2DArray(testRows, testColumns, 10, 99);
+Print2DArray(testArr2D);
